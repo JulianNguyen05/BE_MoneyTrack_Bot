b7935bc9f7e40d5a69730c272844ab8fc3f45220
Julian Nguyen <huutrong.160705@gmail.com>
2025-11-02 23:23:56 +0700

    update

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -1,16 +1,18 @@
 """
-ASGI config for myproject project.
+ASGI config for myproject.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+This file exposes the ASGI callable as a module-level variable named ``application``.
+It is used to serve your project via ASGI servers such as Daphne, Uvicorn, or Hypercorn.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+For more information, see:
+https://docs.djangoproject.com/en/stable/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+# --- Thiết lập môi trường Django ---
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
+# --- Tạo ứng dụng ASGI ---
 application = get_asgi_application()
--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -1,16 +1,17 @@
 """
-WSGI config for myproject project.
+WSGI config for myproject.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+This module exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more information, see:
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+# Set the default Django settings module for the WSGI application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
+# Create the WSGI application
 application = get_wsgi_application()
