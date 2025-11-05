40d77addfb69ccb3542b3359b448a01ea3a9b4e4
Julian Nguyen <huutrong.160705@gmail.com>
2025-11-05 19:03:19 +0700

    update chatbot

--- myproject/urls.py
+++ myproject/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # Dòng này là quan trọng nhất:
     # Nó sẽ "include" tất cả các URL từ app 'api' của bạn
     # và đặt chúng dưới tiền tố 'api/'
-    path('api/', include('api.urls')),
+    path('api/', include('api.urls.main_urls')),
 ]
